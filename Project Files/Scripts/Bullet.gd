9071755935bfe4f2f3530d6c6e39e9ca94657bce
Snakefangox <snakefangox@gmail.com>
2017-10-01 16:27:19 +1300

    Fixed the menu

--- Project Files/Scripts/Bullet.gd
+++ Project Files/Scripts/Bullet.gd
@@ -7,7 +7,7 @@ func _ready():
 	set_max_contacts_reported(3)
 
 func _fixed_process(delta):
-	add_force(Vector2(), get_global_mouse_position() - get_tree().get_current_scene().get_node("playerShip").position)
+	add_force(Vector2(), get_global_mouse_position() - $"../../".position)
 	var bodies=get_colliding_bodies()
 	for i in bodies:
 		if "health" in i:
--- Project Files/Scripts/NewGame.gd
+++ Project Files/Scripts/NewGame.gd
@@ -1,19 +1,14 @@
 extends Button
 
 export(String) var load_scene
-var current_scene
+var currentScene
 
 func _ready():
 	var root = get_tree().get_root()
-	current_scene = root.get_child(root.get_child_count() -1)
-
-func goto_scene(path):
-	current_scene.queue_free()
-	load(load_scene)
-
+	currentScene = root.get_child(root.get_child_count() -1)
 
 func _on_NewGame_button_up():
-	pass
-
-func _process(delta):
-	
+	currentScene.queue_free()
+	var s = ResourceLoader.load(load_scene)
+	currentScene = s.instance()
+	get_tree().get_root().add_child(currentScene)
