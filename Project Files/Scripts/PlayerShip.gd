c17eb6b407879ffd6587d09aaad493d5af24fe3d
Snakefangox <snakefangox@gmail.com>
2017-10-03 11:44:13 +1300

    Fixed bug with bullets

--- Project Files/Scripts/PlayerShip.gd
+++ Project Files/Scripts/PlayerShip.gd
@@ -4,7 +4,9 @@ export(NodePath) var ship
 var speed = Vector2(150,150)
 var shipPos = Vector2()
 
-
+export(String) var gunfire
+var firerate = 1
+var stockFirerate = 25
 
 var offset = Vector2()
 
@@ -20,5 +22,21 @@ func _fixed_process(delta):
 	var motion = offset * delta * speed
 	move_and_slide(-motion)
 	
+	fire()
+
+
 func _process(delta):
 	rotation = ( get_node(".").position.angle_to_point( get_global_mouse_position() ) + deg2rad(-90) )
+
+
+func fire():
+	if(firerate > 1):
+		firerate = firerate - 1
+	
+	if(Input.is_mouse_button_pressed(BUTTON_RIGHT) && firerate < 2):
+		firerate = stockFirerate
+		var scene = load(gunfire)
+		var node = scene.instance()
+		node.global_position = $"./Gun".global_position
+		node.global_rotation = $"./Gun".global_rotation
+		$"/root/World/".add_child(node)
