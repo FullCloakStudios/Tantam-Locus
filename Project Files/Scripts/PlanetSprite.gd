db1ce0c055e1024db1739be81a242fa6ab576a0e
Snakefangox <snakefangox@gmail.com>
2017-10-05 20:18:06 +1300

    Added proper stuff for planets
    Like planertary landings and stuff

--- Project Files/Scripts/PlanetSprite.gd
+++ Project Files/Scripts/PlanetSprite.gd
@@ -6,9 +6,25 @@ const cont = [preload("res://Sprites/PlanetStuff/Cont1.png")]
 
 #const atmo Later mabye
 
+var x
+var i
+
 func _ready():
 	randomize()
 	texture = base[round(rand_range(0,0))] #add to this for more random
 	$"PlanetCon".texture = cont[round(rand_range(0,0))]
 	modulate = Color(rand_range(0,1),rand_range(0,1),rand_range(0,1))
 	$"PlanetCon".modulate = Color(rand_range(0,1),rand_range(0,1),rand_range(0,1))
+
+func _process(delta):
+	if(x != null):
+		x.scale = i / 1.1
+		i = x.scale
+		if(i.x < 0.1):
+			print("do stuff")
+			x = null
+
+func _on_Area2D_body_entered( body ):
+		if(body == $"/root/World/playerShip"):
+			x = body
+			i = body.scale
