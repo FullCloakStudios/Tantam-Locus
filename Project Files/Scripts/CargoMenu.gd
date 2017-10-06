68db6afad3e5295e2b2078e87d14aa06a7ab982a
Snakefangox <snakefangox@gmail.com>
2017-10-07 10:30:23 +1300

    Started working on a item menu

---
+++ Project Files/Scripts/CargoMenu.gd
@@ -0,0 +1,5 @@
+extends ItemList
+
+
+func _on_Menu_about_to_show():
+	add_icon_item(
--- Project Files/Scripts/LSButt.gd
+++ Project Files/Scripts/LSButt.gd
@@ -2,4 +2,4 @@ extends TextureButton
 
 
 func _on_TextureButton_button_up():
-	$"/root/World/SaveManager".saveGame()
+	$"/root/World/UI/PopupPanel".popup(Rect2(160,30,700,500))
