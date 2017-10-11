f6f7c268ced1e7fcec1359a2a2ae901dc4c801e0
Snakefangox <snakefangox@gmail.com>
2017-10-11 13:09:45 +1300

    Fixed menu collider

--- Project Files/Scripts/InventoryPanel.gd
+++ Project Files/Scripts/InventoryPanel.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+var ignoreOnce = false
+
 func invAdd(path):
-	var x = load(path).instance()
-	$"../".add_item(x.get_name(),x.texture, true)
-	$"../../Cargo".drag.queue_free()
-	$"../../Cargo".drag = null
+	if(ignoreOnce == true):
+		ignoreOnce = false
+	else:
+		var x = load(path).instance()
+		$"../".add_item(x.get_name(),x.texture, true)
+		$"../../Cargo".drag.queue_free()
+		$"../../Cargo".drag = null
