d0000d74da35a964db99badf4f1d335ef8c01ab4
Snakefangox <snakefangox@gmail.com>
2017-10-11 13:33:05 +1300

    :snake:  :ox:

--- Project Files/Scripts/Item.gd
+++ Project Files/Scripts/Item.gd
@@ -1,13 +1,15 @@
 extends Sprite
 
+onready var ItemPath = get_filename()
+
 func _on_Area2D_body_entered( body ):
-	if(body.addCargo("res://Prefabs/Items/TestItem.tscn")): 
+	if(body.addCargo(ItemPath)): 
 		queue_free()
 	else:
 		pass
 
 func _on_Area2D_area_entered( area ):
-	if(area.invAdd("res://Prefabs/Items/TestItem.tscn")):
+	if(area.invAdd(ItemPath)):
 		queue_free()
 	else:
 		pass
