250337a38047c7180d80e57589b5f056833352ec
Snakefangox <snakefangox@gmail.com>
2017-10-12 11:26:05 +1300

    WOOOO BUG FIXES
    and sexy scalies

--- Project Files/Scripts/InventoryPanel.gd
+++ Project Files/Scripts/InventoryPanel.gd
@@ -6,7 +6,11 @@ func invAdd(path):
 	if(ignoreOnce == true):
 		ignoreOnce = false
 	else:
+		var c = $"../"
 		var x = load(path).instance()
-		$"../".add_item(x.get_name(),x.texture, true)
+		c.add_item(x.get_name(),x.texture, true)
+		c.set_item_metadata(c.get_item_count() - 1,$"../../Cargo".drag.get_filename())
+		print(c.get_item_count())
+		print( $"../../Cargo".drag.get_filename())
 		$"../../Cargo".drag.queue_free()
 		$"../../Cargo".drag = null
