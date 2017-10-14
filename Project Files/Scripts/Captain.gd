f30898cc04cfc086032dd2f428f099dd24306675
Snakefangox <snakefangox@gmail.com>
2017-10-14 17:56:21 +1300

    Changed captain movement to global

--- Project Files/Scripts/Captain.gd
+++ Project Files/Scripts/Captain.gd
@@ -7,11 +7,11 @@ onready var cam = $"Camera"
 
 func _fixed_process(delta):
 	if(Input.is_mouse_button_pressed(BUTTON_LEFT) && shouldMove):
-		offset = position - get_global_mouse_position()
+		offset = global_position - get_global_mouse_position()
 		var motion = offset * delta * speed
 		move_and_slide(-motion)
 
 
 func _process(delta):
 	if(shouldMove):
-		rotation = ( get_node(".").position.angle_to_point( get_global_mouse_position() ) + deg2rad(-90) )
+		rotation = (global_position.angle_to_point( get_global_mouse_position() ) + deg2rad(-90) )
