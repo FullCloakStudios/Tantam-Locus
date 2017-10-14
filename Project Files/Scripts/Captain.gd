c0ec65900627452b0c4b36d4f14c644d50fa10b6
Snakefangox <snakefangox@gmail.com>
2017-10-14 17:49:29 +1300

    Fixed captain camera

--- Project Files/Scripts/Captain.gd
+++ Project Files/Scripts/Captain.gd
@@ -3,12 +3,14 @@ extends KinematicBody2D
 var speed = 60
 var shouldMove = true
 var offset = Vector2(0,0)
+onready var cam = $"Camera"
 
 func _fixed_process(delta):
 	if(Input.is_mouse_button_pressed(BUTTON_LEFT) && shouldMove):
 		offset = position - get_global_mouse_position()
 		var motion = offset * delta * speed
 		move_and_slide(-motion)
+
 
 func _process(delta):
 	if(shouldMove):
