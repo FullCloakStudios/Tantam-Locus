fdb15184c18d6e508cc86b9f60732578887204ce
Snakefangox <snakefangox@gmail.com>
2017-10-15 15:21:35 +1300

    added random terrain gen

--- Project Files/Scripts/PlanetSurface.gd
+++ Project Files/Scripts/PlanetSurface.gd
@@ -5,6 +5,7 @@ var disCheck
 
 var posArray = PoolVector2Array()
 var objArray = PoolStringArray()
+var possTerrain = [preload("res://Sprites/Tileset2.png"),preload("res://Sprites/Tileset3.png")]
 
 func _ready():
 	randomize()
@@ -14,7 +15,23 @@ func _draw():
 	draw_circle($captain.position, 500, planetModulate)
 	disCheck = $captain.position
 
+func generate(pos, radi):
+	var terr = load("res://Prefabs/Terrain.tscn").instance()
+	terr.texture = possTerrain[rand_range(0,1)]
+	terr.position = pos + Vector2(rand_range(-radi,radi),rand_range(-radi,radi))
+	add_child(terr)
+	
+
 func _process(delta):
 	if(disCheck.distance_to($captain.position) > 200):
 		hide()
 		show()
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
+		generate($captain.position,10)
