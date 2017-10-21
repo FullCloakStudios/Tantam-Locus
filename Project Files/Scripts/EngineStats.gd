7f8eafc0e6857a17238e8b07fb5652c7a48c7ae7
Snakefangox <snakefangox@gmail.com>
2017-10-22 10:54:54 +1300

    Added particle effects

--- Project Files/Scripts/EngineStats.gd
+++ Project Files/Scripts/EngineStats.gd
@@ -4,3 +4,9 @@ export(float) var thrust
 
 func _ready():
 	$"../".speed = thrust 
+
+func _process(delta):
+	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		$"Particles2D".emitting = true
+	else:
+		$"Particles2D".emitting = false
