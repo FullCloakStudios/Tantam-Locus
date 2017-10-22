6ab0a7158f884e790405986dbcbf85821deb075e
Snakefangox <snakefangox@gmail.com>
2017-10-22 15:45:50 +1300

    Fixed a lot of bugs with enemy
    It's worse now

--- Project Files/Prefabs/BaseEnemyShip.gd
+++ Project Files/Prefabs/BaseEnemyShip.gd
@@ -4,16 +4,16 @@ onready var player = $"/root/World/playerShip"
 var health
 var route = Vector2(rand_range(-1,1),rand_range(-1,1))
 var move
-var speed = 1
+var speed = 200
 
 func _physics_process(delta):
-	if(player.position.distance_to(position) > 1):
-		move = position - route
-		rotation = ($".".position.angle_to_point( move ) + deg2rad(90) )
-		move = move + (delta * Vector2(speed,speed))
-		move_and_slide(move)
-	else:
-		move = position - player.position
-		rotation = ($".".position.angle_to_point( player.position ) + deg2rad(-90) )
-		move = move + (delta * Vector2(speed,speed))
+	if(player.position.distance_to(position) > 300):
+		rotation = ($".".position.angle_to_point( position - route ) + deg2rad(-90) )
+		move = route * speed
 		move_and_slide(-move)
+	else:
+		move = player.position
+		print (move)
+		rotation = ($".".position.angle_to_point( player.position ) + deg2rad(-90) )
+		move = move * speed
+		move_and_slide(move)
