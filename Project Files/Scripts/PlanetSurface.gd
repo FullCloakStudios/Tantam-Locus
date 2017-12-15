13e9bbeeed847f810d94032d2fa2dc68a4c04818
Snakefangox <snakefangox@gmail.com>
2017-12-15 13:30:53 +1300

    Added tilesets

--- Project Files/Scripts/PlanetSurface.gd
+++ Project Files/Scripts/PlanetSurface.gd
@@ -14,34 +14,12 @@ func _ready():
 	show()
 	firstPro = true
 
+func _physics_process(delta):
+	pass
+
 
 func _draw():
-	draw_circle(captain.position, 1000, planetModulate)
-	disCheck = captain.position
-	generate(rand_range(20,35),captain.position,100,false)
+	pass
 
 func generate(amount, pos, radi, safe):
-	var index = 0
-	while(index < amount):
-		var terr = possTerrain[rand_range(0,0)].instance()
-		terr.modulate = planetModulate
-		terr.position = pos + Vector2(rand_range(-radi,radi),rand_range(-radi,radi))
-		if(safe):
-			terr.position * 2.5
-		var sc = rand_range(terr.minScale,terr.maxScale)
-		terr.scale = Vector2(sc,sc)
-		terr.rotation = rand_range(0,360)
-		add_child(terr)
-		index += 1
-		randomize()
-	
-
-func _physics_process(delta):
-	if(disCheck.distance_to(captain.position) > 200):
-		hide()
-		show()
-		generate(rand_range(40,69),captain.position,100,true)
-	if(firstPro):
-		var ui = load("res://Prefabs/UI.tscn").instance()
-		$"/root/World".add_child(ui)
-		firstPro = false
+	pass
